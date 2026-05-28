54ac22424af9a1d1803f521fbe2f01897fc0b431
timkorba34 <timkorba@yahoo.com>
2026-05-28 11:29:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.title("Document AI PoC")
+st.set_page_config(page_title="Document AI PoC")
+
+st.title("AI Document Segmentation PoC")
+
+uploaded_file = st.file_uploader(
+    "Upload a scanned PDF",
+    type=["pdf"]
+)
+
+if uploaded_file:
+    st.success("PDF uploaded successfully")
+    
+    st.write("Filename:", uploaded_file.name)
+    st.write("File Size:", uploaded_file.size, "bytes")
